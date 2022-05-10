2edf69c61baa75b69fcb347f4ad4c93645c558da
Kasey <Kastlegate@gmail.com>
2022-05-10 16:45:02 -0400

    changed the tab icon

--- src/components/CardTemplate.js
+++ src/components/CardTemplate.js
@@ -3,8 +3,8 @@ import '../style/Card.css';
 const CardTemplate = (props) => {
 
     return(
-        <div className="card">
-            <img className="gameCover" title={props.name} src={props.src}></img>
+        <div className="card" >
+            <img className="gameCover"  title={props.name} src={props.src}></img>
         </div>
     )
 };
